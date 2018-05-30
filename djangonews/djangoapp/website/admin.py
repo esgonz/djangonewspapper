31547d87cca0ec8eec30f8bd7a38a59008dd7189
esgonz <estebangon.g@gmail.com>
2018-05-30 19:34:30 -0400

    admin configurado

--- djangonews/djangoapp/website/admin.py
+++ djangonews/djangoapp/website/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import News
+from .models import NewsType
+from .models import Category
+from .models import Author
+from .models import Status
+from .models import MediaType
+from .models import Media
+from .models import FeatureMedia
+from .models import Tags
+from .models import NewsTags
+from .models import Gallery
 # Register your models here.
+
+admin.site.register(News)
+admin.site.register(NewsType)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Status)
+admin.site.register(MediaType)
+admin.site.register(Media)
+admin.site.register(FeatureMedia)
+admin.site.register(Tags)
+admin.site.register(NewsTags)
+admin.site.register(Gallery)

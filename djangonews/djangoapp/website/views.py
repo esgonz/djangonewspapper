0dd27beaaeb718ef9f95d82ff9094793ac0b3363
esgonz <estebangon.g@gmail.com>
2018-05-31 00:43:16 -0400

    trabajando vistas

--- djangonews/djangoapp/website/views.py
+++ djangonews/djangoapp/website/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # 
 def index(request):
 	return HttpResponse("Hellow, world. You are in a website news") 
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at news %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of news %s."
+    return HttpResponse(response % question_id)
+
+def eduardo(request, age):
+	response ="Hola Eduardo tu edad es %d"
+	return HttpResponse(response %age)
